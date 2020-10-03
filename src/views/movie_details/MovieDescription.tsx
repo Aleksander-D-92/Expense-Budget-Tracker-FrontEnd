b344d1272340325acc3bd08387adb802393046ce
Aleksander D <aleksander.dorkov@gmail.com>
2020-10-03 12:33:43 +0300

    Can now add to favorite from tv show details and movie details

--- src/views/movie_details/MovieDescription.tsx
+++ src/views/movie_details/MovieDescription.tsx
@@ -1,13 +1,75 @@
-import React from "react";
-import {Card, CardContent, CardHeader, Grid, Typography} from "@material-ui/core";
+import React, {MouseEvent} from "react";
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CircularProgress,
+    Grid,
+    IconButton,
+    Tooltip,
+    Typography
+} from "@material-ui/core";
 import {MovieDetails} from "../../services/the_movie_db/MovieService";
 import {formatDate} from "../../shared/utils/functions";
+import {useDispatch, useSelector} from "react-redux";
+import {ReduxState} from "../../config/redux/ReduxStore";
+import {
+    CREATE_FAVORITE,
+    CreateFavoriteResp,
+    CreateFavoriteVars, DELETE_FAVORITE, DeleteFavoriteVars,
+    FavoriteType
+} from "../../services/apollo/mutations/FavoriteMutations";
+import {useMutation} from "@apollo/client";
+import {Dummy} from "../../services/apollo/ApoloConfig";
+import {addFavoriteAction, deleteFavoriteAction} from "../../config/redux/Favorites";
+import AddIcon from "@material-ui/icons/Add";
+import CloseIcon from "@material-ui/icons/Close";
 
 interface Props {
     movieDetails?: MovieDetails
 }
 
 function MovieDescription(props: Props) {
+    const userId = useSelector((state: ReduxState) => state.userDetails.userId);
+    const favoriteMovies = useSelector((state: ReduxState) => state.favorites.filter(f => f.favoriteType === FavoriteType.MOVIE));
+    const dispatch = useDispatch();
+    const [createFavorite, {data, loading: createLoading}] = useMutation<CreateFavoriteResp, CreateFavoriteVars>(CREATE_FAVORITE);
+    const [deleteFavorite, {loading: deleteLoading}] = useMutation<Dummy, DeleteFavoriteVars>(DELETE_FAVORITE)
+
+    function addFavorite(e: MouseEvent) {
+        createFavorite({
+            variables: {
+                userId: userId,
+                movieDBId: parseInt(e.currentTarget.id),
+                favoriteType: FavoriteType.MOVIE
+            }
+        }).then((e) => {
+            if (e.data === null || e.data === undefined) {
+                return;
+            }
+            dispatch(addFavoriteAction(e.data.createFavorite));
+        })
+    }
+
+    function removeFavorite(e: MouseEvent) {
+        let currentTargetId = e.currentTarget.id;
+        // @ts-ignore
+        const id = favoriteMovies.find(f => f.movieDBId == parseInt(currentTargetId)).favoriteId;
+        if (id === undefined) {
+            return;
+        }
+        deleteFavorite({
+            variables:
+                {
+                    favoriteId: id
+                }
+        }).then(() => {
+            let fav = favoriteMovies.find(f => f.favoriteId == id);
+            if (fav !== undefined) {
+                dispatch(deleteFavoriteAction(fav))
+            }
+        })
+    }
     return (
         <>
             <Grid container={true} justify={'center'}>
@@ -15,6 +77,43 @@ function MovieDescription(props: Props) {
                     <Card elevation={10} className={'mt-2'}>
                         <CardHeader
                             title="Movie Info"
+                            action={
+                                <>
+                                    {!(favoriteMovies.filter(f => f.movieDBId == props.movieDetails?.id && f.favoriteType === FavoriteType.MOVIE).length > 0) ?
+                                        <>
+                                            <IconButton aria-label="settings"
+                                                        style={{display: (deleteLoading || createLoading) ? 'none' : 'inline-flex'}}
+                                                        onClick={addFavorite}
+                                                        id={`${props.movieDetails?.id}`}>
+                                                <Tooltip title={"Click to Add to favorites"}
+                                                         placement={'top'}
+                                                         arrow={true}>
+                                                    <AddIcon fontSize={'large'}/>
+                                                </Tooltip>
+                                            </IconButton>
+                                            <CircularProgress size={40}
+                                                              thickness={15}
+                                                              style={{display: (deleteLoading || createLoading) ? 'block' : 'none'}}/>
+                                        </>
+                                        :
+                                        <>
+                                            <IconButton aria-label="settings"
+                                                        id={`${props.movieDetails?.id}`}
+                                                        style={{display: (deleteLoading || createLoading) ? 'none' : 'inline-flex'}}
+                                                        onClick={(e: MouseEvent) => removeFavorite(e)}>
+                                                <Tooltip title={"Click to Remove from favorites"}
+                                                         placement={'top'}
+                                                         arrow={true}>
+                                                    <CloseIcon fontSize={'large'}/>
+                                                </Tooltip>
+                                            </IconButton>
+                                            <CircularProgress size={45}
+                                                              thickness={15}
+                                                              style={{display: (deleteLoading || createLoading) ? 'block' : 'none'}}/>
+                                        </>
+                                    }
+                                </>
+                            }
                         />
                         <CardContent>
                             <Typography variant={'h6'}>
--- src/views/tv_show_details/TvShowDescription.tsx
+++ src/views/tv_show_details/TvShowDescription.tsx
@@ -1,13 +1,41 @@
-import React, {useEffect, useState} from "react";
+import React, {MouseEvent, useEffect, useState} from "react";
 import {TVShowDetails} from "../../services/the_movie_db/TvShowsService";
-import {Card, CardContent, CardHeader, Grid, Typography} from "@material-ui/core";
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CircularProgress,
+    Grid,
+    IconButton,
+    Tooltip,
+    Typography
+} from "@material-ui/core";
 import {formatDate} from "../../shared/utils/functions";
+import {useDispatch, useSelector} from "react-redux";
+import {ReduxState} from "../../config/redux/ReduxStore";
+import {
+    CREATE_FAVORITE,
+    CreateFavoriteResp,
+    CreateFavoriteVars, DELETE_FAVORITE, DeleteFavoriteVars,
+    FavoriteType
+} from "../../services/apollo/mutations/FavoriteMutations";
+import {useMutation} from "@apollo/client";
+import {Dummy} from "../../services/apollo/ApoloConfig";
+import {addFavoriteAction, deleteFavoriteAction} from "../../config/redux/Favorites";
+import AddIcon from "@material-ui/icons/Add";
+import CloseIcon from "@material-ui/icons/Close";
 
 interface Props {
     tvShowDetails?: TVShowDetails
 }
 
 function TvShowDescription(props: Props) {
+    const userId = useSelector((state: ReduxState) => state.userDetails.userId);
+    const favoriteTV = useSelector((state: ReduxState) => state.favorites.filter(f => f.favoriteType === FavoriteType.TV));
+    const dispatch = useDispatch();
+    const [createFavorite, {data, loading: createLoading}] = useMutation<CreateFavoriteResp, CreateFavoriteVars>(CREATE_FAVORITE);
+    const [deleteFavorite, {loading: deleteLoading}] = useMutation<Dummy, DeleteFavoriteVars>(DELETE_FAVORITE)
+
     const [totalEpisodes, setTotalEpisodes] = useState<number>(0)
     useEffect(() => {
         let count = 0;
@@ -16,6 +44,43 @@ function TvShowDescription(props: Props) {
         })
         setTotalEpisodes(count);
     }, [props.tvShowDetails])
+
+    function addFavorite(e: MouseEvent) {
+        createFavorite({
+            variables: {
+                userId: userId,
+                movieDBId: parseInt(e.currentTarget.id),
+                favoriteType: FavoriteType.TV
+            }
+        }).then((e) => {
+            if (e.data === null || e.data === undefined) {
+                return;
+            }
+            dispatch(addFavoriteAction(e.data.createFavorite));
+        })
+    }
+
+    function removeFavorite(e: MouseEvent) {
+        let currentTargetId = e.currentTarget.id;
+        console.log(currentTargetId);
+        // @ts-ignore
+        const id = favoriteTV.find(f => f.movieDBId == parseInt(currentTargetId)).favoriteId;
+        if (id === undefined) {
+            return;
+        }
+        deleteFavorite({
+            variables:
+                {
+                    favoriteId: id
+                }
+        }).then(() => {
+            let fav = favoriteTV.find(f => f.favoriteId == id);
+            if (fav !== undefined) {
+                dispatch(deleteFavoriteAction(fav))
+            }
+        })
+    }
+
     return (
         <>
             <Grid container={true} justify={'center'}>
@@ -23,6 +88,43 @@ function TvShowDescription(props: Props) {
                     <Card elevation={10} className={'mt-2'}>
                         <CardHeader
                             title="Tv Show Info"
+                            action={
+                                <>
+                                    {!(favoriteTV.filter(f => f.movieDBId == props.tvShowDetails?.id && f.favoriteType === FavoriteType.TV).length > 0) ?
+                                        <>
+                                            <IconButton aria-label="settings"
+                                                        style={{display: (deleteLoading || createLoading ) ? 'none' : 'inline-flex'}}
+                                                        onClick={addFavorite}
+                                                        id={`${props.tvShowDetails?.id}`}>
+                                                <Tooltip title={"Click to Add to favorites"}
+                                                         placement={'top'}
+                                                         arrow={true}>
+                                                    <AddIcon fontSize={'large'}/>
+                                                </Tooltip>
+                                            </IconButton>
+                                            <CircularProgress size={40}
+                                                              thickness={15}
+                                                              style={{display: (deleteLoading || createLoading ) ? 'block' : 'none'}}/>
+                                        </>
+                                        :
+                                        <>
+                                            <IconButton aria-label="settings"
+                                                        id={`${props.tvShowDetails?.id}`}
+                                                        style={{display: (deleteLoading || createLoading ) ? 'none' : 'inline-flex'}}
+                                                        onClick={(e: MouseEvent) => removeFavorite(e)}>
+                                                <Tooltip title={"Click to Remove from favorites"}
+                                                         placement={'top'}
+                                                         arrow={true}>
+                                                    <CloseIcon fontSize={'large'}/>
+                                                </Tooltip>
+                                            </IconButton>
+                                            <CircularProgress size={40}
+                                                              thickness={45}
+                                                              style={{display: (deleteLoading || createLoading) ? 'block' : 'none'}}/>
+                                        </>
+                                    }
+                                </>
+                            }
                         />
                         <CardContent>
                             <Typography variant={'h6'}>
