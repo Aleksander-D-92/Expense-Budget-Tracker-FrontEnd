b0ce0751150936ce8f0ce532c1f097bcbf18ed4c
Aleksander D <aleksander.dorkov@gmail.com>
2020-10-03 21:02:08 +0300

    Routes are now protected

--- src/config/react-router-dom/WebsiteRoutes.tsx
+++ src/config/react-router-dom/WebsiteRoutes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
+import {useSelector} from 'react-redux';
+import {Redirect, Route, Switch} from 'react-router-dom';
 import {UserRegisterController} from "../../views/user/register/UserRegisterController2";
 import {UserLoginController} from "../../views/user/login/UserLoginController";
 import {UserLogoutController} from "../../views/user/logout/UserLogoutController";
@@ -11,24 +12,44 @@ import {MovieDetailsController} from "../../views/movie_details/MovieDetailsCont
 import {TvShowDetailsController} from "../../views/tv_show_details/TvShowDetailsController";
 import {ActorDetailsController} from "../../views/actor_details/ActorDetailsController";
 import {FavoriteController} from "../../views/favorites/FavoriteController";
+import {ReduxState} from "../redux/ReduxStore";
 
 function WebsiteRoutes() {
+    const ROLE_ADMIN = 'ROLE_ADMIN';
+    const authority = useSelector((state: ReduxState) => state.userDetails.authority);
+    const userLoggedIn = useSelector((state: ReduxState) => state.userLoggedIn);
     return (
         <Switch>
-            <Route exact={true} path={'/'}><LandingPageController/></Route>
-            <Route exact={true} path={'/users/login'}>
-                {console.log('v rautera sme')}
-                <UserLoginController/>
+            <Route exact={true} path={'/'}>
+                <LandingPageController/>
             </Route>
-            <Route exact={true} path={'/users/register'}><UserRegisterController/></Route>
-            <Route exact={true} path={'/users/logout'}><UserLogoutController/></Route>
-            <Route exact={true} path={'/users/account-settings'}><AccountSettingsController/></Route>
-            <Route exact={true} path={'/admins/all-users'}><AllUsersController/></Route>
-            <Route exact={true} path={'/admins/edit-user/:userId'}><AdminEditUserController/></Route>
+            <Route exact={true} path={'/users/login'}>
+                {(userLoggedIn) ? <Redirect to={'/'}/> : <UserLoginController/>}
+            </Route>
+            <Route exact={true} path={'/users/register'}>
+                {(userLoggedIn) ? <Redirect to={'/'}/> : <UserRegisterController/>}
+            </Route>
+            <Route exact={true} path={'/users/logout'}>
+                <UserLogoutController/>
+            </Route>
+            <Route exact={true} path={'/users/account-settings'}>
+                <AccountSettingsController/>
+            </Route>
+            <Route exact={true} path={'/admins/all-users'}>
+                {(authority === ROLE_ADMIN) ? <AllUsersController/> : <Redirect to={'/'}/>}
+            </Route>
+            <Route exact={true} path={'/admins/edit-user/:userId'}>
+                {(authority === ROLE_ADMIN) ? <AdminEditUserController/> : <Redirect to={'/'}/>}
+            </Route>
             <Route exact={true} path={'/movies/:movieId'}><MovieDetailsController/></Route>
             <Route exact={true} path={'/tv-shows/:tvShowId'}><TvShowDetailsController/></Route>
             <Route exact={true} path={'/actors/:actorId'}><ActorDetailsController/></Route>
-            <Route exact={true} path={'/favorites'}><FavoriteController/></Route>
+            <Route exact={true} path={'/favorites'}>
+                {(userLoggedIn) ? <FavoriteController/> : <Redirect to={'/users/login'}/>}
+            </Route>
+            <Route path="*">
+                <Redirect to={'/'}/>
+            </Route>
         </Switch>
     )
 }
